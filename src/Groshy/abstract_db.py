9a1db90950fd3ccfc239d5ab5daf35491aeecf0d
Ty <freemanty27@gmail.com>
2023-10-13 08:12:29 -0400

    Created ABC for databases. Adds db directories and files if needed. Cookbook now a child class. May need to switch away from vanilla json db

---
+++ src/Groshy/abstract_db.py
@@ -0,0 +1,104 @@
+from pathlib import Path
+import json
+
+
+class AbstractDB:
+    def __init__(self, db_name: str, db_type: str):
+        cwd = Path.cwd()
+        db_root = Path.joinpath(cwd, ".dbs")
+        self.name = db_name  # Define database name from argument
+        self.type = db_type
+        self.dir = Path.joinpath(db_root, self.type)
+        self.path = Path.joinpath(self.dir, f"{self.name}.json")
+
+        if not db_root.exists():
+            Path.mkdir(db_root)
+            Path.mkdir(self.dir)
+        elif db_root.exists() and not self.dir.exists():
+            Path.mkdir(self.dir)
+
+        if self.build_db():  # Build new database
+            print(f"{self.name} ready for use")
+        else:
+            print(f"{self.name} could not be built")
+            self.db_remove()
+
+    def build_db(self):
+
+        msg = {self.name: {}}  # Message to print in new database file
+        result = False  # Default return value for this method
+
+        try:
+            with open(self.path, "x") as db:  # Create database json file and dump message
+                json.dump(msg, db, indent=4)
+                print(f"{self.name} created successfully")
+                result = True  # Signal creation of db
+        except FileExistsError:
+            print(f"A cookbook named {self.name} already exists in location {self.dir}...")
+
+            ans_flg = False
+            attempts = 3
+            while ans_flg is False and attempts > 0:
+                resp = input("Would you like to choose a different name? (y/n)")
+
+                if resp == "y":
+                    ans_flg = True
+                    self.name = input("Enter new name: ")
+                    result = self.build_db()
+                elif resp == "n":
+                    ans_flg = True
+                    print("Cookbook creation aborted")
+                else:
+                    print("Unexpected input, please try again.")
+                    attempts -= 1
+                    continue
+
+        return result
+
+    def db_write(self, msg):
+
+        success = False
+        try:
+            with open(self.path, "w") as db:
+                json.dump(msg, db, indent=4)
+            success = True
+        except FileNotFoundError:
+            print(f"No database file found. Double check this location: {self.path}.json")
+            pass
+
+        return success
+
+    def db_add(self, msg):
+
+        success = False
+        try:
+            with open(self.path, "a") as db:
+                json.dump(msg, db, indent=4)
+            success = True
+        except FileNotFoundError:
+            print(f"No database file found. Double check this location: {self.path}.json")
+            pass
+
+        return success
+
+    def db_read(self):
+        contents = False
+        try:
+            with open(self.path, "w") as db:
+                json.load(db)
+        except FileNotFoundError:
+            print(f"No database file found. Double check this location: {self.path}.json")
+            pass
+
+        return contents
+
+    def db_remove(self):
+        success = False
+        print(f"Removing {self.name} from {self.dir}")
+        Path.unlink(self.path)
+        if not self.path.exists():
+            success = True
+            print(f"{self.name} removed...")
+
+        return success
+
--- src/Groshy/cookbook_builder.py
+++ src/Groshy/cookbook_builder.py
@@ -2,59 +2,25 @@ import os
 import json
 
 from recipe import Recipe
+from abstract_db import AbstractDB
 
 
-class CookBook:
-    def __init__(self, db_name: str, db_dir: str):
-        
+class CookBook(AbstractDB):
+    def __init__(self, db_name: str = "Cookbook1"):
+        super().__init__(db_name, "cookbook")
+
+
 def save_recipe(recipe: Recipe, db_name: str):
 
-    with open(f"db_path")
-    recipe.to_dict()
-
-
-
-def build_db(db_path: str, name: str= "Cookbook", categories: list= ["Breakfast", "Lunch", "Dinner", "Snacks"]):
-
-    if db_path.endswith("/"):
-        db_file = f"{db_path}{name}.json"
-    else:
-        db_file = f"{db_path}/{name}.json"
-
-    cat_dict = dict.fromkeys(categories, {})
-    msg = {name: cat_dict}
-
-    result = False
-
-    try:
-        with open(db_file, "x") as db:
-            json.dump(msg, db, indent= 4)
-            print(f"{name} cookbook created successfully")
-            result = True
-    except FileExistsError:
-        print(f"A cookbook named {name} already exists...")
-
-        resp = input("Would you like to choose a new name? (y/n)")
-
-        if resp == "y":
-            name = input("Enter new cookbook name: ")
-            build_db(db_path, name, categories)
-            result = True
-        else:
-            print("Cookbook creation aborted")
-
-    return result
-
+    with open(f"db_path") as db:
+        recipe.to_dict()
 
 
 if __name__ == "__main__":
 
-    dbs = "./.dbs"
+    babys_first_cookbook = CookBook("bb1ckbk")
 
-    if build_db(dbs, "new"):
-        with open(f"{dbs}/new.json", "r") as f:
-            skeleton = json.load(f)
-    else:
-        skeleton = "Well Shit"
-
-    print(skeleton)
+    print(babys_first_cookbook.name)
+    print(babys_first_cookbook.type)
+    print(babys_first_cookbook.dir)
+    print(babys_first_cookbook.path)
