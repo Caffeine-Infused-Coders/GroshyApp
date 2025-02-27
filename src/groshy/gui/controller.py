942ea4abd0368e1de53fa287810e045d13acd484
Ty Freeman <freemanty27@gmail.com>
2025-02-26 22:14:45 -0500

    Add docstrings to controller.py functions

--- src/groshy/gui/controller.py
+++ src/groshy/gui/controller.py
@@ -12,6 +12,7 @@ from groshy.gui.recipe_screen import RecipeScreen
 
 class Controller(ScreenManager):
     def __init__(self, **kwargs):
+        """Subclass of ScreenManager acts as controller for GUI."""
         super().__init__(**kwargs)
         self.cookbooks = (
             ListProperty()
@@ -20,7 +21,13 @@ class Controller(ScreenManager):
     def add_screen(
         self, screen_type: str, name: str, change: bool, new: bool, dt: int = 2
     ):
-        """Adds a new screen of type `screen_type` to the screenmanager"""
+        """Adds a new screen of type `screen_type` to the screenmanager
+        Args:
+            :param screen_type (str) The type of screen to add to Controller, i.e. bookshelf, recipe, cookbook
+            :param name (str) Name of new screen
+            :param change (bool) Flag to enable transition to screen after creation
+            :param new (bool) Flag indicating the creation of a new db as opposed to opening one
+            :param dt (int) Delay for transition to new screen"""
 
         new_widget = None
 
@@ -44,14 +51,24 @@ class Controller(ScreenManager):
             return True
 
     def create_new_cookbook(self):
+        """Creates and opens a Popup form for creating a new cookbook db"""
+
         cookbook_creator = CookBookForm(self)
         cookbook_creator.open()
 
     def create_new_recipe(self):
+        """Creates and opens a Popup form for creating a new Recipe in a cookbook db"""
+
         recipe_creator = RecipeForm(self)
         recipe_creator.open()
 
     def wait_for_trans(self, new_screen, dt=2):
+        """Schedules the transition to a new screen on a delay (dt)
+
+        Args:
+        :param new_screen (str) Name of screen to transition to.
+        :param dt (int | float) Duration of delay in seconds. [defaults to 2]"""
+
         def _change_current(nscreen, dt):
             self.current = nscreen
 
